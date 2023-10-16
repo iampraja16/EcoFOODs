460c42bd1db629731f5e31ae368d1cd37a98d2ae
iampraja16 <148161020+iampraja16@users.noreply.github.com>
2023-10-17 05:44:00 +0700

    Add files via upload

---
+++ EcoFoods/src/User.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author A S U S
+ */
+public class User {
+    private String nama;
+    private String email;
+    private String password;
+    private String alamat;
+    private String nomorTelp;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
